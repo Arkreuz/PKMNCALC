04124a4ee9263cc92ee336f8cf7062179d0f814e
Arkreuz <coder@arkreuz.com>
2017-01-08 03:45:16 -0600

    First commit

---
+++ script_res/setdex_tt2017.js
@@ -0,0 +1,49 @@
+var SETDEX_TT2017={
+  "Swellow": {
+    "Common Showdown": {
+      "level": 50,
+      "evs": {
+        "hp": 0,
+        "at": 252,
+        "df": 0,
+        "sa": 0,
+        "sd": 4,
+        "sp": 252
+      },
+      "nature": "Jolly",
+      "ability": "Guts",
+      "item": "Flame Orb",
+      "moves": [
+        "Brave Bird",
+        "Facade",
+        "Quick Attack",
+        "U-turn"
+      ]
+    }
+  },
+
+  "Pyukumuku": {
+    "Arkreuz's Pyukumuku": {
+      "level": 100,
+      "evs": {
+        "hp": 252,
+        "at": 0,
+        "df": 64,
+        "sa": 0,
+        "sd": 188,
+        "sp": 6
+      },
+      "nature": "Impish",
+      "ability": "Unaware",
+      "item": "Leftovers",
+      "moves": [
+        "Toxic",
+        "Soak",
+        "Protect",
+        "Recover"
+      ]
+    }
+  },
+
+
+};

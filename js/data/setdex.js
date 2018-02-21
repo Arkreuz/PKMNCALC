80eadfa4d3cdb40f3e980ce4f745cf86c8213433
Arkreuz <developer@arkreuz.com>
2018-02-21 03:00:15 -0600

    210218A Setdex Dugtrio Added

--- js/data/setdex.js
+++ js/data/setdex.js
@@ -595,6 +595,29 @@ var SETDEX= {
             ]
         },
     },
+        //051
+    "Dugtrio": {
+        "Trapper": {
+            "level": 100,
+            "evs": {
+                "hp":6,
+                "at": 252,
+                "df": 0,
+                "sa":0,
+                "sd": 0,
+                "sp":252
+            },
+            "nature": "Jolly",
+            "ability": "Arena Trap",
+            "item": "Groundium Z",
+            "moves": [
+                "Sucker Punch",
+                "Stone Edge",
+                "Earthquake",
+                ""
+            ]
+        },
+    },
     //057
     "Primeape": {
         "Wagner": {

e153f60552544c18ba33ed8da67f77029c2a8e45
Arkreuz <developer@arkreuz.com>
2018-09-01 02:48:48 -0500

    010918 Watchog added to setdex

--- js/data/setdex.js
+++ js/data/setdex.js
@@ -8039,6 +8039,29 @@ var SETDEX= {
                         ]
                 },
         },
+                //505
+        "Watchog": {
+                "All-Out Attacker": {
+                        "level": 100,
+                        "evs": {
+                                "hp":0,
+                                "at": 252,
+                                "df": 4,
+                                "sa":0,
+                                "sd": 0,
+                                "sp":252
+                        },
+                        "nature": "Adamant",
+                        "ability": "Analytic",
+                        "item": "Life Orb",
+                        "moves": [
+                                "Return",
+                                "Low Kick",
+                                "Knock Off",
+                                "Seed Bomb"
+                        ]
+                },
+        },
         //508
         "Stoutland": {
                 "Wolfgang": {
